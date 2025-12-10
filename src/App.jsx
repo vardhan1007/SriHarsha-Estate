74eb8baf3d56415721e64e98e0f5f315ae2bb44f
vardhan1007 <svardhan1007@gmail.com>
2025-12-10 06:58:21 +0530

    creating pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import SignUp from "./pages/SignUp"
+import SignIn from "./pages/signin"
+import About from "./pages/about"
+import Profile from "./pages/Profile"
+import Home from "./pages/home"
 export default function App() {
   return (
-    <div className="min-h-screen bg-red-500 text-white flex items-center justify-center text-4xl">
-      Hello Tailwind
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/signin" element={<SignIn/>} />
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/profile"element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
