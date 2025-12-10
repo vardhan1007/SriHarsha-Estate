4d838e8f2af22ee0a40cbe6513869fb5343768e9
vardhan1007 <svardhan1007@gmail.com>
2025-12-10 05:57:55 +0530

    install react js and tailwind css and create the first template

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-red-500 text-white flex items-center justify-center text-4xl">
+      Hello Tailwind
+    </div>
+  )
+}
+
+
